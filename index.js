7e432b5a63e8292d5983ae4f5869ba887383d6a1
Bruno Mendes <brunomendes962@gmail.com>
2021-03-08 09:47:46 -0300

    Aula 1 - 3

---
+++ index.js
@@ -0,0 +1,10 @@
+var n1 = document.querySelector("#n1");
+var n2 = document.querySelector("#n2");
+var btn = document.querySelector("#btnSum");
+function sum(a, b) {
+    return a + b;
+}
+btn.addEventListener("click", function (event) {
+    var res = sum(Number(n1.value), Number(n2.value));
+    console.log(res);
+});
---
+++ index.ts
@@ -0,0 +1,12 @@
+const n1 = document.querySelector("#n1") as HTMLInputElement;
+const n2 = document.querySelector("#n2") as HTMLInputElement;
+const btn = document.querySelector("#btnSum");
+
+function sum(a: number, b: number) {
+  return a + b;
+}
+
+btn.addEventListener("click", (event) => {
+  const res = sum(Number(n1.value), Number(n2.value));
+  console.log(res);
+});
